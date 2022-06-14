c68b04d9b140dc81077bda206ab89e76cd28dfd0
favour-121 <favourmogbonjubola@gmail.com>
2022-06-14 09:40:27 +0100

    printing hello world in c programming

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -starts the code
+ * description -prints hello world
+ * retutn 0
+ */
+int main(void)
+{
+	printf("HELLO WORLD THIS IS MY REPO\n");
+
+	return (0);
+}
