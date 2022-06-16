c860f65ebc8c1559253267ec7677a07bf34dc90a
favour-121 <favourmogbonjubola@gmail.com>
2022-06-16 02:00:34 +0100

    Working with function

---
+++ function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -Startes the program
+ * Descrption - Working with functions
+ */
+void greet()
+{
+	printf("Good Morning");
+}
+int main()
+{
+	greet();
+
+	return (0);
+}
