0491f9f6261a2d03773f563d7bf1bedfa9f08117
favour-121 <favourmogbonjubola@gmail.com>
2022-06-21 05:09:53 +0100

    Sizes of data types on my system

---
+++ grade.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * Main - peinting grades of a school, by receiving user input 
+ * Return :Always 0
+ */
+int main(void)
+{
+	int main grade;
+
+	printf("ENTER YOUR GRADE AS GOTTEN IN YOUR TEST\n")'
+		scanf("%d" ,&grade);
+	if (grade <= 100 && grade >79)
+	{
+		printf("YOUR GRADE IS A\n")
+	}
+	else if (grade 
---
+++ size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	printf("size of type 'char' on my system : %lu bytes\n" , sizeof(char));
+	 printf("size of type 'double' on my system : %lu bytes\n" , sizeof(double));
+	  printf("size of type 'float' on my system : %lu bytes\n" , sizeof(float));
+	   printf("size of type 'int' on my system : %lu bytes\n" , sizeof(int));
+	    
+	return(0);
+}
